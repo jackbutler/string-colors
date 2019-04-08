d42eeb5e5d6311c377884ad845f2f6d4998712ac
Jack Butler <jack@jackbutler.co>
2019-04-08 08:20:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
+import md5 from 'md5';
 import logo from './logo.svg';
 import './App.css';
 
+const divStyle = {
+  color: '#ffffff',
+  display: 'inline-block',
+  width: '20%',
+  padding: '10px',
+}
+
+const Ascii = ({ children, sat, light }) => {
+  const color = Math.round(360 * parseInt(md5(children), 16) / Math.pow(16, 32));
+  console.log(color);
+  return (<div style={{ backgroundColor: `hsl(${color}, ${sat}%, ${light}%)`, ...divStyle}}>
+    {children}
+  </div>)
+}
+
+
+const categories = ['Category 1, Category 2, Category 3, Category 4']
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      sat: 50,
+      light: 50,
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          Saturation ({this.state.sat})
+        <input type="range" min="0" max="100" value={this.state.sat} onChange={e => this.setState({ sat: e.target.value })} class="slider" id="myRange"/>
+        </div>
+        <div style={{ paddingBottom: '30px'}}>
+        Lightness ({this.state.light})
+        <input type="range" min="0" max="100" value={this.state.light} onChange={e => this.setState({ light: e.target.value })} class="slider" id="myRange"/>
+        </div>
+        {categories.map(c => <Ascii sat={this.state.sat} light={this.state.light} >{c}</Ascii>)}
       </div>
     );
   }
